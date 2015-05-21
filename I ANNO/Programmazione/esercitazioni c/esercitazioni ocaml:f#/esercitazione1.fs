225c31f1c8ad10b9265088052b64de2896977f8e
fundor333 <fundor333@gmail.com>
2015-05-21 15:56:59 +0200

    Aggiornamento complessivo

---
+++ I ANNO/Programmazione/esercitazioni c/esercitazioni ocaml:f#/esercitazione1.fs
@@ -0,0 +1,41 @@
+﻿// Ulteriori informazioni su F# all'indirizzo http://fsharp.net
+
+module esercitazione1
+
+(*
+ESERCIZIO 1
+
+Secondo il libro “L’Arte della Cucina”, uno sciroppo andrebbe scaldato ad un grado in meno rispetto alla temperatura indicata per ogni 150 metri di altezza sul livello
+del mare.
+
+Definire la funzione candy_temperature che dati due argomenti, la temperatura originale in gradi e l’altezza in metri sul livello del mare, calcola la temperatura
+ da usare per la cottura. 
+ Per esempio, la ricetta per il Caramello al Cioccolato richiede una temperature di 118 gradi; supponiamo di volerla cucinare ad Enna, il capoluogo più alto d’Italia 
+ (948 metri sul livello del mare). Usare la funzione candy_temperature per calcolare la temperatura da usare.
+
+FIRMA: candy_temperature : float -> float -> float
+*)
+
+let candy_temperature t h = t - (h/150);;
+
+(*ESERCIZIO 2
+
+Modificare la funzione candy_temperature in candy_temperature2 affinché arrotondi il risultato all’intero più vicino, usando la funzione di arrotondamento round
+ (già integrata nel linguaggio F#).
+
+FIRMA: candy_temperature2 : float -> float -> float*)
+
+let candy_temperature t h = round(t - (h/150.00));;
+
+(*ESERCIZIO 3
+
+“L’Arte della Cucina” suggerisce un modo per calcolare per quante persone basterà un tacchino. 
+Per tacchini fino a 5kg potranno essere fatte porzioni di 300g per persona, mentre per tacchini più grossi la dose si riduce a solo 200g. 
+Scrivere la funzione turkey che prende in input il peso di un tacchino e calcola il numero di persone per cui questo sarà sufficiente.
+
+FIRMA: turkey : int -> int
+*)
+
+let turkey w =
+    if w<=5000 then w/300 else w/200;;
+
---
+++ I ANNO/Programmazione/esercitazioni c/esercitazioni ocamlf#/esercitazione7.fs
@@ -0,0 +1,93 @@
+﻿//Minati Leonardo - Esercitazione 7
+
+module esercitazione7
+
+type Direzione = Su | Giu | Destra | Sinistra;;
+
+type Colore = Bianco | Nero;;
+
+type Posizione = Pos of int * char;;
+
+//esercizio 1
+let pedone color position = match color with
+                            Bianco -> match position with 
+                                      (2,x) -> (4,x)
+                                      |(x,y) -> //check if the next position is valid. if it isn't then return the original position
+                                                if x <> 8 then (x+1,y)
+                                                else (x,y)
+                            |Nero -> match position with
+                                     (7,x) -> (5,x)
+                                     |(x,y) -> //check if the next position is valid. if it isn't then return the original position
+                                              if x <> 1 then (x-1,y)
+                                              else (x,y)
+
+//esercizio 2
+let rec alfiere position verticalDirection horizontalDirection cells = match verticalDirection, horizontalDirection with
+                                                                       Su, Destra -> //check if the position is valid
+                                                                             match position with
+                                                                             (x,'h') -> (*invalid position: return the original position*)position
+                                                                             |(8,y) -> (*invalid position: return the original position*)position
+                                                                             |(x,y) ->  if cells > 0 then 
+                                                                                            match y with
+                                                                                            'a' -> alfiere (x+1, 'b') Su Destra (cells-1)
+                                                                                            |'b' -> alfiere (x+1, 'c') Su Destra (cells-1)
+                                                                                            |'c' -> alfiere (x+1, 'd') Su Destra (cells-1)
+                                                                                            |'d' -> alfiere (x+1, 'e') Su Destra (cells-1)
+                                                                                            |'e' -> alfiere (x+1, 'f') Su Destra (cells-1)
+                                                                                            |'f' -> alfiere (x+1, 'g') Su Destra (cells-1)
+                                                                                            |'g' -> alfiere (x+1, 'h') Su Destra (cells-1)
+                                                                                        else position
+                                                                      |Su, Sinistra -> //check if the position is valid
+                                                                             match position with
+                                                                             (x,'a') -> (*invalid position: return the original position*)position
+                                                                             |(8,y) -> (*invalid position: return the original position*)position
+                                                                             |(x,y) ->  if cells > 0 then 
+                                                                                            match y with
+                                                                                            'b' -> alfiere (x+1, 'a') Su Sinistra (cells-1)
+                                                                                            |'c' -> alfiere (x+1, 'b') Su Sinistra (cells-1)
+                                                                                            |'d' -> alfiere (x+1, 'c') Su Sinistra (cells-1)
+                                                                                            |'e' -> alfiere (x+1, 'd') Su Sinistra (cells-1)
+                                                                                            |'f' -> alfiere (x+1, 'e') Su Sinistra (cells-1)
+                                                                                            |'g' -> alfiere (x+1, 'f') Su Sinistra (cells-1)
+                                                                                            |'h' -> alfiere (x+1, 'g') Su Sinistra (cells-1)
+                                                                                        else position
+                                                                      |Giu, Destra -> //check if the position is valid
+                                                                             match position with
+                                                                             (x,'h') -> (*invalid position: return the original position*)position
+                                                                             |(1,y) -> (*invalid position: return the original position*)position
+                                                                             |(x,y) ->  if cells > 0 then 
+                                                                                            match y with
+                                                                                            'a' -> alfiere (x-1, 'b') Su Destra (cells-1)
+                                                                                            |'b' -> alfiere (x-1, 'c') Su Destra (cells-1)
+                                                                                            |'c' -> alfiere (x-1, 'd') Su Destra (cells-1)
+                                                                                            |'d' -> alfiere (x-1, 'e') Su Destra (cells-1)
+                                                                                            |'e' -> alfiere (x-1, 'f') Su Destra (cells-1)
+                                                                                            |'f' -> alfiere (x-1, 'g') Su Destra (cells-1)
+                                                                                            |'g' -> alfiere (x-1, 'h') Su Destra (cells-1)
+                                                                                        else position
+                                                                      |Giu, Sinistra -> //check if the position is valid
+                                                                             match position with
+                                                                             (x,'a') -> (*invalid position: return the original position*)position
+                                                                             |(1,y) -> (*invalid position: return the original position*)position
+                                                                             |(x,y) ->  if cells > 0 then 
+                                                                                            match y with
+                                                                                            'b' -> alfiere (x-1, 'a') Su Sinistra (cells-1)
+                                                                                            |'c' -> alfiere (x-1, 'b') Su Sinistra (cells-1)
+                                                                                            |'d' -> alfiere (x-1, 'c') Su Sinistra (cells-1)
+                                                                                            |'e' -> alfiere (x-1, 'd') Su Sinistra (cells-1)
+                                                                                            |'f' -> alfiere (x-1, 'e') Su Sinistra (cells-1)
+                                                                                            |'g' -> alfiere (x-1, 'f') Su Sinistra (cells-1)
+                                                                                            |'h' -> alfiere (x-1, 'g') Su Sinistra (cells-1)
+                                                                                        else position
+
+//esercizio 3
+
+//funzione ausiliaria per controllare se una posizione è valida sulla scacchiera
+let isValid pos = match pos with
+                  (x,y) -> if (x >= 1 && x <= 8) && (y = 'a' || y = 'b' || y = 'c' || y = 'd' || y = 'e' || y = 'f' || y = 'g' || y = 'h') then true
+                           else false 
+
+
+let cavallo pos1 pos2 = match pos1 ,pos2 with 
+                        (a,b) ,(c,d) -> if ((c = a+2 && (int) d = (int) b+1) && isValid (c,d)) || ((c = a+2 && (int) d = (int) b-1) && isValid (c,d)) || ((c = a-2 && (int) d = (int) b-1) && isValid (c,d)) || ((c = a-2 && (int) d = (int) b+1) && isValid (c,d)) || ((c = a+1 && (int) d = (int) b+2) && isValid (c,d)) || ((c = a-1 && (int) d = (int) b+2) && isValid (c,d)) || ((c = a+1 && (int) d = (int) b-2) && isValid (c,d)) || ((c = a-1 && (int) d = (int) b-2) && isValid (c,d)) then true 
+                                        else false
